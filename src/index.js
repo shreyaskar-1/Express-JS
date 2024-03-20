c3a5d15b9a01ede187a97c0fadd41465cdf65d9d
shreyaskar-1 <shreyaskar_219031@gmail.com>
2024-03-21 03:43:40 +0530

    addons

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express();
+const path = require("path");
+
+const staticPath = path.join(__dirname,"../public")
+
+
+// builtin middleware
+app.use(express.static(staticPath))
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.get('/about', function (req, res) {
+//     res.send('Ram Ram Ladder')
+//   })
+
+app.listen(3000)
