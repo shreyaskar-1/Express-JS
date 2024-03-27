5ebfa9cf32cba42bb84e51497f5b95f82f1c5f08
shreyaskar-1 <shreyaskar_219031@gmail.com>
2024-03-28 03:55:59 +0530

    addon

--- src/index2.js
+++ src/index2.js
@@ -1,23 +1,37 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const hbs = require("hbs")
 
 // Built-in middleware
 const static = path.join(__dirname, "../public2");
-// how to change the name of views
-
-const template = path.join(__dirname, "../src/templates");
+const views = path.join(__dirname, "templates", "views");
+const partials = path.join(__dirname, "templates","partials");
 
 
 // setting view engine
 app.set("view engine","hbs");
-app.set("views",template);
+app.set("views",views);
+hbs.registerPartials(partials);
 
 // app.use(express.static(static));
 
 // template engine route
 app.get("/", (req, res) => {
     res.render("index",{dynamic:"whassup"});
+});
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/about/*", (req, res) => {
+    res.render("404",
+    {error:"this about us page does not exist"});
+});
+
+app.get("*", (req, res) => {
+    res.render("404",
+    {error:"you hit an error page buddy"});
 });
 
 app.get("/", (req, res) => {
