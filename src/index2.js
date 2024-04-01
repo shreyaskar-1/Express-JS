66195b1e9b93e7bc938cd13f80c647e92e49fedf
shreyaskar-1 <shreyaskar_219031@gmail.com>
2024-04-01 10:05:53 +0530

    api practice set

--- src/index2.js
+++ src/index2.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const hbs = require("hbs")
+const requests = require("requests");
 
 // Built-in middleware
 const static = path.join(__dirname, "../public2");
@@ -21,7 +22,25 @@ app.get("/", (req, res) => {
     res.render("index",{dynamic:"whassup"});
 });
 app.get("/about", (req, res) => {
-    res.render("about");
+    requests(`https://api.openweathermap.org/data/2.5/weather?q=${req.query.name}&appid=609f40e66cc25bd1959951ffce8b5818`)
+    .on('data', (chunk) => {
+            const objdata = JSON.parse(chunk);
+            console.log("Response from API:", objdata); 
+            // if (objdata && objdata.name && objdata.main && objdata.main.temp) {
+                console.log(`City name is ${objdata.name} and the temp is ${objdata.main.temp}`);
+                res.write(objdata.name);
+            // } else {
+            //     console.log("Invalid response format:", objdata);
+            //     res.write("Invalid data received from the API");
+            // }
+    })
+    .on('end', (err) => {
+        if (err) {
+            console.log('Connection closed due to errors', err);
+            res.write("Error processing API response");
+        }
+        res.end();
+    });
 });
 
 app.get("/about/*", (req, res) => {
